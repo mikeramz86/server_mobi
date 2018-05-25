59af18f75dc49cc6e1cf418a4989763f70d59dde
Mike <mikeramzdesign@gmail.com>
2018-05-24 18:29:11 -0700

    added models folder and updated users.js

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+/*-----------------------------------------Create New User (POST ENDPOINT)------------------------------------------------ */
+
+
+/*-----------------------------------------Update User (PUT ENDPOINT)------------------------------------------------ */
+
+
+

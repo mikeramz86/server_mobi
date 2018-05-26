a4b7b59dec3b864819629f813b0fe0750374c8e5
Mike <mikeramzdesign@gmail.com>
2018-05-25 23:57:22 -0700

    added models jobs - added config - added routes jobs

---
+++ routes/jobs.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const passport = require('passport');
+const { Jobs } = require('../models/jobs');
+
+//----GET
+
+router.get('/', (req, res) => {
+    res.json(Jobs.get());
+  });
+
+//---POST
+
+router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ['job', 'company', 'status', 'date', 'comp', 'pros', 'cons', 'notes'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+    const item = Jobs.create(req.body.job, req.body.company,req.body.status,req.body.date,req.body.comp,req.body.pros,req.body.cons,req.body.notes);
+    res.status(201).json(item);
+  });
+
+//---PUT
+
+//---DELETE
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,98 @@
+//server
 'use strict';
-
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cors = require('cors');
+
+var path = require('path');
+// var logger = require('morgan');
+var users = require('./routes/users');
+// var logged_in = require('./routes/logged_in');
+var jobs = require('./routes/jobs');
+
+
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  next();
+});
 
-if (require.main === module) {
-  app.listen(process.env.PORT || 8080, function () {
-    console.info(`App listening on ${this.address().port}`);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/auth/', authRouter);
+app.use('/users', users);
+// app.use('/logged_in', logged_in);
+app.use('/api/jobs', jobs)
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
+});
+
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+let server;
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
   });
 }
-
-module.exports = app;
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+module.exports = { app, runServer, closeServer };

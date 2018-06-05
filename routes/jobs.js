e95e88093ed3192b1a1a52e51f881c6097f504ad
Mike <mikeramzdesign@gmail.com>
2018-06-05 16:08:45 -0700

    updates on npm

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,10 +7,6 @@ const { Job } = require('../models/jobs');
 
 //----GET
 
-// router.get('/', (req, res) => {
-//   res.json(Job.get());
-// });
-
 router.get('/', (req, res) => {
   try {
     Job.find({}).then(alljobs => {
@@ -25,7 +21,6 @@ router.get('/', (req, res) => {
 
 router.post('/', jsonParser, (req, res) => {
   const requiredFields = ['job', 'company', 'stage', 'status', 'date', 'comp', 'pros', 'cons', 'notes'];
-  console.log(requiredFields);
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -38,7 +33,7 @@ router.post('/', jsonParser, (req, res) => {
     job:req.body.job, 
     company: req.body.company, 
     stage: req.body.stage, 
-    satus: req.body.status, 
+    status: req.body.status, 
     date: req.body.date, 
     comp: req.body.comp, 
     pros: req.body.pros, 
@@ -52,8 +47,62 @@ router.post('/', jsonParser, (req, res) => {
 });
 
 //---PUT
+router.put('/:id', jsonParser, (req, res) => {
+  console.log('is this working');
+  const requiredFields = [ 'job', 'company', 'stage', 'status', 'date', 'comp', 'pros', 'cons', 'notes'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+        `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating job with id \`${req.params.id}\``);
+
+  const updatedItem = {
+    id: req.params.id,
+    job: req.body.job, 
+    company: req.body.company, 
+    stage: req.body.stage, 
+    status: req.body.status, 
+    date: req.body.date, 
+    comp: req.body.comp, 
+    pros: req.body.pros, 
+    cons: req.body.cons, 
+    notes: req.body.notes
+  };
+
+  Job.findByIdAndUpdate(req.body.id, updatedItem, { new: true })
+  .then(updatedItem => {
+    console.log(updatedItem);
+    res.status(201).json(updatedItem.serialize())
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ error: "ughhhhhhhh no no" });
+  });
+});
 
 //---DELETE
+
+router.delete("/:id", (req, res) => {
+  Job.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).json({ message: "success, my friend!" });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "ughhhhhhhh no no" });
+    });
+});
 
 
 module.exports = router;

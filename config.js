c204f48656b515c2351c630d45a415f3022353e1
Mike <mikeramzdesign@gmail.com>
2018-10-09 20:51:44 -0700

    updated npm

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/projDante';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test';
 exports.PORT = process.env.PORT || 8080
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
